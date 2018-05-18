35cbacf5b3a25258b19462ccdd87622f366f9fca
guzele <giseleGarifullina@gmail.com>
2018-05-18 22:13:34 +0300

    Added first examples for genFunStmt

---
+++ stmtFromFuncExamples/arrayClear-1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+void combineMeasurements (vector<int> &res, vector<int> &vec, vector<int> &vec2, set<int> &values);
+
+int main() {
+    vector<int> res, a, b;
+    set<int> s;
+
+    a.clear();
+    b.clear();
+    s.clear();
+
+    a.push_back(1);
+    a.push_back(2);
+
+    s.insert(3);
+    s.insert(4);
+
+    b.push_back(5);
+
+    //res.clear();
+    combineMeasurements(res,a,b,s);
+    for (std::vector<int>::const_iterator elem = res.begin(); elem != res.end(); ++elem){
+        std::cout << *elem << ' ';
+
+    }
+    return 0;
+}
+void addSetToVector (vector<int> &res, set<int> &values){
+    copy(values.begin(), values.end(), std::back_inserter(res));
+}
+
+void addVectorToVector (vector<int> &res, vector<int> &values){
+    //delete
+    res.clear();
+    copy(values.begin(), values.end(), std::back_inserter(res));
+}
+void combineMeasurements (vector<int> &res, vector<int> &vec, vector<int> &vec2, set<int> &values){
+    addVectorToVector (res,vec);
+    addSetToVector(res,values);
+    addVectorToVector (res,vec2);
+}
---
+++ stmtFromFuncExamples/readFromFile-2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <dirent.h>
+#include <sys/stat.h>
+
+bool is_file(const char* path) {
+    struct stat buf;
+    stat(path, &buf);
+    return S_ISREG(buf.st_mode);
+}
+
+bool is_dir(const char* path) {
+    struct stat buf;
+    stat(path, &buf);
+    return S_ISDIR(buf.st_mode);
+}
+
+
+enum ERROR { NO = 0, OPEN_FILE , OPEN_DIR };
+
+ERROR error = ERROR::NO;
+
+void readFromFile(std::string path, std::string name);;
+void readFromDir(std::string path);
+
+int main() {
+    readFromDir("/home/guzel/1");
+    std::cout << error << std::endl;
+
+    error = ERROR::NO;
+    readFromFile("/home/guzel/1", "1.txt");
+    std::cout << error << std::endl;
+    return 0;
+}
+
+void readFromDir(std::string path) {
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir (path.c_str())) != NULL) {
+        while ((ent = readdir (dir)) != NULL) {
+            std::string file = ent->d_name;
+            if (file != "." && file != ".."){
+                readFromFile(path, file);
+            }
+        }
+        closedir (dir);
+    } else {
+        error = ERROR::OPEN_DIR;
+    }
+}
+
+void readFromFile(std::string path, std::string name) {
+    std::string fullName = path + '/' + name;
+    std::string line;
+    std::string res = "";
+    std::ifstream myfile (fullName);
+    if (is_file(fullName.c_str())&& myfile.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+            res += line + "\n";
+        }
+        myfile.close();
+    }
+    else {
+        error = ERROR::OPEN_FILE;
+    }
+    std::cout << res << std::endl;
+
+};
+
